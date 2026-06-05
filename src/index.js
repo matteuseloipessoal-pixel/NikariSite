b96b070092bef43b03deb57c8db87a9f29dadbf2
NikariSite <matteuseloipessoal@gmail.com>
2026-06-05 13:10:20 -0300

    Adicionando vercel.json e corrigindo rotas

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { builder } from "@builder.io/react";
+
+builder.init("89d08ef748224d42b7bd865e074a81ab");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
